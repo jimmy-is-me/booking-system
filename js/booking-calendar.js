4f89bfc10f8ef9aa2214efdafcc21d547e044f2f
jimmy-is-me <76391627+jimmy-is-me@users.noreply.github.com>
2026-02-05 17:29:16 +0800

    Add booking modal functionality and event handling

--- js/booking-calendar.js
+++ js/booking-calendar.js
@@ -11,16 +11,71 @@ jQuery(document).ready(function($) {
                     center: 'title',
                     right: 'dayGridMonth,timeGridWeek,timeGridDay'
                 },
+                buttonText: {
+                    today: '今天',
+                    month: '月',
+                    week: '週',
+                    day: '日'
+                },
                 events: bookingCalendarData.bookings,
                 eventClick: function(info) {
                     info.jsEvent.preventDefault();
-                    if (info.event.url) {
-                        window.open(info.event.url, '_blank');
-                    }
+                    
+                    var bookingId = info.event.extendedProps.bookingId;
+                    
+                    // 顯示彈出視窗
+                    showBookingModal(bookingId);
+                },
+                eventTimeFormat: {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: false
                 }
             });
             
             calendar.render();
         }
     }
+    
+    // 顯示預約詳情彈出視窗
+    function showBookingModal(bookingId) {
+        var modal = $('#booking-modal');
+        var modalBody = $('#booking-modal-body');
+        
+        modalBody.html('<p>載入中...</p>');
+        modal.show();
+        
+        $.ajax({
+            url: bookingCalendarData.ajaxurl,
+            type: 'POST',
+            data: {
+                action: 'get_booking_details',
+                nonce: bookingCalendarData.nonce,
+                booking_id: bookingId
+            },
+            success: function(response) {
+                if (response.success) {
+                    modalBody.html(response.data.html);
+                    $('#booking-edit-link').attr('href', response.data.edit_url);
+                } else {
+                    modalBody.html('<p class="error">' + response.data.message + '</p>');
+                }
+            },
+            error: function() {
+                modalBody.html('<p class="error">載入失敗</p>');
+            }
+        });
+    }
+    
+    // 關閉彈出視窗
+    $('.booking-modal-close, #booking-modal-close-btn').on('click', function() {
+        $('#booking-modal').hide();
+    });
+    
+    // 點擊背景關閉
+    $('#booking-modal').on('click', function(e) {
+        if (e.target === this) {
+            $(this).hide();
+        }
+    });
 });
